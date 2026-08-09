72fe2435e9f09706fb41cf509188ebd46e22b9a0
Shobhit Raghuwanshi <shobhit822000@gmail.com>
2026-08-09 22:10:08 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int j = 0;
+        
+        if (s1.size() > s2.size()) return false;
+
+        sort(s1.begin(), s1.end());
+
+        for(int i = 0 ; i <= (int)s2.size() - (int)s1.size(); i++){
+
+            string s = s2.substr(i , s1.size());
+
+            sort(s.begin(), s.end());
+
+            if(s == s1){
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+};
